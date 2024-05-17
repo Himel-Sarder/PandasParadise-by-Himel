728cd033209b470e8b10086ada7608384a8ce940
Himel Sarder <143216886+Himel-Sarder@users.noreply.github.com>
2024-05-18 00:41:19 +0600

    Delete Input-Output Tools directory

--- Input-Output Tools/Reading CSV File.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('Samples/Test.csv')
-
-print(df.head(3))
-print(df.tail())
-print(df.describe())
-print(df['Grade'])
--- Input-Output Tools/Reading Excel File.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-path = "Samples/Test3.xlsx"
-
-df = pd.read_excel(path,
-                   sheet_name="Employee Sample Data")
-
-print(df.head())
